6ecb9d5bb7154adead220ee2dab214b6c505023c
Lucas Lopes <lucaslops12@gmail.com>
2026-03-01 14:18:35 -0300

    complete exercise 4

--- MaratonaProgramacao/src/main/java/Exercise4.java
+++ MaratonaProgramacao/src/main/java/Exercise4.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Exercise4 {
-//    Um novo robô de limpeza para um grande salão retangular está sendo desenvolvido.
+    //    Um novo robô de limpeza para um grande salão retangular está sendo desenvolvido.
 //    O robô vai percorrer o caminho definido por uma linha marcada no chão, que é coberto com ladrilhos quadrados, brancos e pretos: ladrilhos pretos indicam o caminho que o robô deve percorrer.
 //    Ao movimentar-se, o robô pode andar apenas em linha reta, para a frente. Parado, o robô pode girar para as quatro direções (Norte, Sul, Leste e Oeste).
 //
@@ -22,6 +24,60 @@ public class Exercise4 {
 //    1≤B≤C1≤B≤C
 //    A posição final é diferente da posição inicial.
     public static void main(String[] args) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
 
+        Scanner sc = new Scanner(System.in);
+
+        int L = sc.nextInt();
+        int C = sc.nextInt();
+        int A = sc.nextInt() - 1;
+        int B = sc.nextInt() - 1;
+
+        int[][] matriz = new int[L][C];
+
+        for (int i = 0; i < L; i++) {
+            for (int j = 0; j < C; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        // Variáveis para acompanhar a posição atual e a posição anterior do robô
+        int atualX = A;
+        int atualY = B;
+
+        int anteriorX = -1;
+        int anteriorY = -1;
+
+        // Loop para percorrer o caminho do robô até encontrar a posição final
+        while (true) {
+            int proxX = -1;
+            int proxY = -1;
+            // Verifica os quatro vizinhos do ladrilho atual para encontrar o próximo ladrilho preto
+            for (int i = 0; i < 4; i++) {
+                int nx = atualX + dx[i];
+                int ny = atualY + dy[i];
+
+                if (nx >= 0 && nx < L && ny >= 0 && ny < C) {
+                    if (matriz[nx][ny] == 1) {
+                        if (nx != anteriorX || ny != anteriorY) {
+                            proxX = nx;
+                            proxY = ny;
+                        }
+                    }
+                }
+            }
+
+            if (proxX == -1) {
+                break;
+            }
+
+            anteriorX = atualX;
+            anteriorY = atualY;
+            atualX = proxX;
+            atualY = proxY;
+        }
+
+        System.out.println((atualX + 1) + " " + (atualY + 1));
     }
 }
+
